caaa9bd5c6a3ba58883517c16c1a9da990fab014
fabianoobispo1 <fabianoobispo@icloud.com>
2024-11-04 19:35:56 -0300

    first commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,12 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://fluent-gecko-11.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
+
+export default authConfig;
+
+
